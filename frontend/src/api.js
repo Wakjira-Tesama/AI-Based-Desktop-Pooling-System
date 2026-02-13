928266d16662f664a5000cd23c52ef2aae59e247
Wakjira-Tesama <wakjiratesama@gmail.com>
2026-02-13 19:05:00 +0300

    Send device ID header in API client

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,8 +8,12 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
+    const deviceId = localStorage.getItem('device_uuid');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
+    }
+    if (deviceId) {
+      config.headers['X-Device-Id'] = deviceId;
     }
     return config;
   },
